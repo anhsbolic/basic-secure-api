1783000b39cafaae89a773c994c805e7624f951e
Anhar Solehudin <anhsbolic@gmail.com>
2019-11-24 18:32:39 +0700

    Bad Update Profile

--- database/migrations/2019_11_24_095620_create_user_profiles_table.php
+++ database/migrations/2019_11_24_095620_create_user_profiles_table.php
@@ -17,10 +17,10 @@ class CreateUserProfilesTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('avatar');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('gender');
+            $table->string('avatar')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('gender')->nullable();
             $table->timestamps();
         });
     }
