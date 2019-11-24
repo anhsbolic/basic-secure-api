013f20b343234229f82e18189936d6c74c723637
Anhar Solehudin <anhsbolic@gmail.com>
2019-11-24 17:41:02 +0700

    Bad Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\UserProfile;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -17,7 +19,22 @@ class ProfileController extends Controller
     }
 
     public function profile($id_user) {
+        // find user  
+        $user = User::find($id_user);
 
+        // find user profile
+        $profile = UserProfile::find($id_user);
+
+        // response
+        $response = [
+            'code' => 2,
+            'message' => 'User Profile',
+            'data' => [
+                'user' => $user,
+                'profile' => $profile,
+            ]
+        ];
+        return response()->json($response, 200);
     }
 
     public function updateProfile(Request $request, $id_user) {
